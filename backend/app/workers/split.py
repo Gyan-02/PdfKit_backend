bfac0a363c8de37e90b8726d329bf6e132252df9
Gyan <gyangaurav02@gmail.com>
2026-06-03 19:40:37 +0530

    Rebuild git index

--- backend/app/workers/split.py
+++ backend/app/workers/split.py
@@ -1,8 +1,8 @@
-
 import fitz
 
 from pathlib import Path
 from datetime import datetime, UTC
+
 from app.core.celery_app import celery_app
 from app.db.session import SessionLocal
 from app.models.job import Job
@@ -13,37 +13,37 @@ from app.models.file import File
 def split_pdf_task(job_id: int):
 
     db = SessionLocal()
+    job = None
 
     try:
+
         job = (
             db.query(Job)
             .filter(Job.id == job_id)
             .first()
         )
-         
- 
+
         if not job:
             return
-        
+
         file_id = job.options["file_id"]
+
         print(file_id)
-        
+
         job.status = "processing"
         db.commit()
 
         print(f"Split Job {job_id} Started")
 
-        
-
         db_file = (
             db.query(File)
-            .filter(File.id ==(file_id))
-            .all()
+            .filter(File.id == file_id)
+            .first()
         )
 
         if not db_file:
             raise Exception("File not found")
-        
+
         input_path = db_file.s3_key
 
         doc = fitz.open(input_path)
@@ -56,23 +56,22 @@ def split_pdf_task(job_id: int):
             new_pdf = fitz.open()
 
             new_pdf.insert_pdf(
-            doc,
-            from_page=page_num,
-            to_page=page_num
+                doc,
+                from_page=page_num,
+                to_page=page_num
             )
-            
+
             page_path = (
                 output_dir /
                 f"page_{page_num + 1}.pdf"
             )
 
             new_pdf.save(str(page_path))
-
             new_pdf.close()
-        doc.close()    
 
+        doc.close()
 
-        job.output_file_key= str(output_dir)
+        job.output_file_key = str(output_dir)
         job.status = "completed"
         job.completed_at = datetime.now(UTC)
 
@@ -84,11 +83,10 @@ def split_pdf_task(job_id: int):
 
         print(f"Split Job {job_id} Failed: {e}")
 
-        job.status = "failed"
-        job.error_message = str(e)
-
-        db.commit()
-
+        if job:
+            job.status = "failed"
+            job.error_message = str(e)
+            db.commit()
 
     finally:
         db.close()
